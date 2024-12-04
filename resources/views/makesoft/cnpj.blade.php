aeeef299fc28d98a01137e2381ef09c39724b40f
asllow <arthurmir0429@gmail.com>
2024-12-04 18:20:28 -0300

    First Changes

--- resources/views/makesoft/cnpj.blade.php
+++ resources/views/makesoft/cnpj.blade.php
@@ -4,20 +4,45 @@
 @endsection
 
 @section('content')
-    <iframe src="https://docs.google.com/viewer?url=https://makesoft.net.br/public/pdf/cartaoCNPJ.pdf&embedded=true"
-            style="width:1000px; height:600px;" frameborder="0">
-    </iframe>
+    <div id="test-id-1" style="text-align: center; width: 100%; height: 600px" class="embed-pdf"
+         data-url="{insert_pdf_link_here}"><span class="loader">Please wait...</span></div>
     <script>
-        let newValue = "https://docs.google.com/viewer?url=https://makesoft.net.br/public/pdf/cartaoCNPJ.pdf&embedded=true";
-        let $docViewer = $(`<iframe src="${newValue}" height="100%" width="100%"></iframe>`);
-        //If using modern browser, use and embed object
-        if (window.chrome || typeof (window.mozInnerScreenX) != "undefined")
-            $docViewer = $(`<object width="100%" height="100%" data="${newValue}" type="application/pdf">
-                    <embed src="${newValue}" type="application/pdf">
-                        <p>This browser does not support PDFs.Please download the PDF to view it: <a href="${newValue}">Download PDF</a>.</p>
-                    </embed>
-                </object>`);
-        //Add the new viewer
-        $docViewer.appendTo($("#invoicePreview"));
+        $(document).ready(function () {
+
+            let embed_pdfs = {};
+
+            $('.embed-pdf').each(function () {
+                var $pdfViewer = $('<iframe src="https://docs.google.com/viewer?url=' + $(this).data('url') + '&embedded=true" style="width: 100%; height: 100%" frameborder="0" scrolling="no"></iframe>');
+                $pdfViewer.appendTo($(this));
+                console.log($(this).attr('id') + " created");
+                embed_pdfs[$(this).attr('id')] = 'created';
+            });
+
+            $(document).find('.embed-pdf iframe').load(function () {
+                embed_pdfs[$(this).parents('.embed-pdf').attr('id')] = 'loaded';
+                $(this).siblings('.loader').remove();
+                console.log($(this).parents('.embed-pdf').attr('id') + " loaded");
+            });
+
+            let embed_pdf_check = setInterval(function () {
+                let remaining_embeds = 0;
+                $.each(embed_pdfs, function (key, value) {
+                    try {
+                        if ($($('#' + key)).find('iframe').contents().find("body").contents().length == 0) {
+                            remaining_embeds++;
+                            console.log(key + " resetting");
+                            $($('#' + key)).find('iframe').attr('src', src = 'https://docs.google.com/viewer?url=' + $('#' + key).data('url') + '&embedded=true');
+                        }
+                    } catch (err) {
+                        console.log(key + " reloading");
+                    }
+                });
+
+                if (!remaining_embeds) {
+                    clearInterval(embed_pdf_check);
+                }
+            }, 1000);
+        });
+
     </script>
 @endsection
