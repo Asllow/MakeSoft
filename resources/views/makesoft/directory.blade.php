095ee85f376852493e2cec329efd0160727748ec
asllow <arthurmir0429@gmail.com>
2024-12-03 21:53:33 -0300

    First Changes

--- resources/views/makesoft/directory.blade.php
+++ resources/views/makesoft/directory.blade.php
@@ -6,6 +6,8 @@
 @section('content')
     <div style="margin-bottom: 10px;">
         <h1>Bem Vindo!</h1>
+        <p>Nós somos a Makesoft Solutions, ou apenas Makesoft, a primeira empresa júnior do CEFET-MG unidade
+            Divinópolis</p>
         <p>Somos especializados em fornecer serviços de corte e gravação à laser e impressão 3D para atender às suas
             necessidades.<br/>Com uma equipe experiente e tecnologia de ponta, estamos prontos para transformar suas
             ideias em realidade.<br/>Entre em contato conosco para saber mais sobre como podemos ajudar em seus
