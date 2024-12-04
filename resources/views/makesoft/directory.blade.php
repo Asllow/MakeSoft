244a7bf8100c14d15d3824fd9638c7bd35ce7d76
asllow <arthurmir0429@gmail.com>
2024-12-03 21:51:51 -0300

    First Changes

--- resources/views/makesoft/directory.blade.php
+++ resources/views/makesoft/directory.blade.php
@@ -5,6 +5,7 @@
 
 @section('content')
     <div style="margin-bottom: 10px;">
+        <h1>Bem Vindo!</h1>
         <p>Somos especializados em fornecer serviços de corte e gravação à laser e impressão 3D para atender às suas
             necessidades.<br/>Com uma equipe experiente e tecnologia de ponta, estamos prontos para transformar suas
             ideias em realidade.<br/>Entre em contato conosco para saber mais sobre como podemos ajudar em seus
