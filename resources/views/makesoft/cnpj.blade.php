6abf224ed009b9c363800802816163303f734619
asllow <arthurmir0429@gmail.com>
2024-12-04 14:07:10 -0300

    First Changes

--- resources/views/makesoft/cnpj.blade.php
+++ resources/views/makesoft/cnpj.blade.php
@@ -4,9 +4,7 @@
 @endsection
 
 @section('content')
-    <iframe src="{{ asset('public/laraview/#../pdf/cartaoCNPJ.pdf') }}" width="1000px" height="600px" allowfullscreen
-            webkitallowfullscreen></iframe>
     <embed
         src="https://drive.google.com/viewerng/viewer?embedded=true&url=https://makesoft.net.br/public/pdf/cartaoCNPJ.pdf"
-           width="500" height="375">
+        width="1000px" height="600px">
 @endsection
