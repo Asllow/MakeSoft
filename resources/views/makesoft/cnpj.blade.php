81a43969f54b669d6fa135daa6769d4655936443
asllow <arthurmir0429@gmail.com>
2024-12-04 17:38:14 -0300

    First Changes

--- resources/views/makesoft/cnpj.blade.php
+++ resources/views/makesoft/cnpj.blade.php
@@ -4,9 +4,7 @@
 @endsection
 
 @section('content')
-    <embed
-        src="https://drive.google.com/viewerng/viewer?embedded=true&url=https://makesoft.net.br/public/pdf/cartaoCNPJ.pdf"
-        width="1000px" height="600px">
     <iframe src="https://docs.google.com/viewer?url=https://makesoft.net.br/public/pdf/cartaoCNPJ.pdf&embedded=true"
-            style="width:600px; height:500px;" frameborder="0"></iframe>
+            style="width:1000px; height:600px;" frameborder="0">
+    </iframe>
 @endsection
