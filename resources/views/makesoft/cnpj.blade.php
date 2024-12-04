846430fe169f4412a10377b5298f2c3a7ea24a95
asllow <arthurmir0429@gmail.com>
2024-12-04 18:38:07 -0300

    First Changes

--- resources/views/makesoft/cnpj.blade.php
+++ resources/views/makesoft/cnpj.blade.php
@@ -15,23 +15,31 @@
 
             let embed_pdfs = {};
 
+            // Criação dos visualizadores de PDF
             $('.embed-pdf').each(function () {
                 const $pdfViewer = $('<iframe src="https://docs.google.com/viewer?url=' + $(this).data('url') + '&embedded=true" style="width: 100%; height: 100%" frameborder="0" scrolling="no"></iframe>');
                 $pdfViewer.appendTo($(this));
                 console.log($(this).attr('id') + " created");
                 embed_pdfs[$(this).attr('id')] = 'created';
+
+                // Configurar evento de carregamento do iframe
+                $pdfViewer.on('load', function () {
+                    embed_pdfs[$(this).parent('.embed-pdf').attr('id')] = 'loaded';
+                    $(this).siblings('.loader').remove(); // Remove o loader, se existir
+
+                    // Oculta o elemento com id específico
+                    $('#loading').css('display', 'none');
+
+                    console.log($(this).parent('.embed-pdf').attr('id') + " loaded");
+                });
             });
 
-            $(document).find('.embed-pdf iframe').load(function () {
-                embed_pdfs[$(this).parents('.embed-pdf').attr('id')] = 'loaded';
-                $(this).siblings('.loader').remove();
-                console.log($(this).parents('.embed-pdf').attr('id') + " loaded");
-            });
-
+            // Monitoramento e tentativa de recarregar PDFs com falha
             let embed_pdf_check = setInterval(function () {
                 let remaining_embeds = 0;
                 $.each(embed_pdfs, function (key) {
                     try {
+                        // Verifica se o iframe está vazio
                         if ($($('#' + key)).find('iframe').contents().find("body").contents().length === 0) {
                             remaining_embeds++;
                             console.log(key + " resetting");
@@ -43,7 +51,7 @@
                 });
 
                 if (!remaining_embeds) {
-                    clearInterval(embed_pdf_check);
+                    clearInterval(embed_pdf_check); // Interrompe o intervalo quando todos os PDFs carregaram
                 }
             }, 1000);
         });
