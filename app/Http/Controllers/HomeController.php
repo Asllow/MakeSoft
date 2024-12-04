d3aecc4296e77c0334c2ad2a8b17293c7d2202af
asllow <arthurmir0429@gmail.com>
2024-12-03 21:10:59 -0300

    First Changes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -70,7 +70,7 @@ class HomeController extends Controller
             }
         }
 
-        return view('makesoft.pix', compact('product', 'out', 'pix'));
+        echo $out;
     }
 
     private function montaPix($px): string
--- resources/views/makesoft/pix.blade.php
+++ resources/views/makesoft/pix.blade.php
@@ -6,6 +6,7 @@
 @section('content')
     <div class='container'>
         <div class='pix-item'>
+            <img src='{{ $out }}' alt='PIX'>
             <h5>R${{ $product->preco_produto }}</h5>
             <p style='width: 100%;'>{{ $pix }}</p>
         </div>
