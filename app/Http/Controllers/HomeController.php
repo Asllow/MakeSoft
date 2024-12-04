8de7fab1c7792d464b94f0ba929ef236280aa2c2
asllow <arthurmir0429@gmail.com>
2024-12-03 21:06:54 -0300

    First Changes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -70,7 +70,7 @@ class HomeController extends Controller
             }
         }
 
-        return view('makesoft.pix', compact('product', 'out', 'pix'));
+        return view('makesoft.pix');
     }
 
     private function montaPix($px): string
