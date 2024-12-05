a4a5cbc575c57ee33d038f45f0a7b665eea19c60
asllow <arthurmir0429@gmail.com>
2024-12-05 16:26:32 -0300

    First Changes

--- resources/views/era2d2/index.blade.php
+++ resources/views/era2d2/index.blade.php
@@ -28,5 +28,23 @@
             </div>
         </section>
         <div class="divider1"></div>
+        <section class="section" id="equipe">
+            <div class="container grid">
+                <h2 class="title">A Equipe</h2>
+                <div class="description">
+                    <img alt="" src="">
+                    <div class="text">
+                        <p>Somos o ERA2D2, uma equipe de robótica formada por estudantes do ensino médio do CEFET-MG, em
+                            Divinópolis, Minas Gerais. Nossa jornada começou no início de 2023, dentro do grupo Estudos
+                            de Robótica e Automação (ERA), com o sonho de competir na categoria High do Torneio Brasil
+                            de Robótica (TBR).</p>
+                        <p>O nome ERA2D2 nasceu em uma de nossas primeiras reuniões, quando decidimos unir nossa
+                            identidade com o ERA à paixão de muitos de nós por Star Wars. Assim, escolhemos homenagear o
+                            icônico e carismático R2D2, que agora também inspira nossa criatividade e espírito de
+                            equipe.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
     </main>
 @endsection
