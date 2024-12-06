3b4d072192b2e0ffc96a9391d640711d8df6b7be
asllow <arthurmir0429@gmail.com>
2024-12-06 13:22:06 -0300

    First Changes

--- resources/views/era2d2/header.blade.php
+++ resources/views/era2d2/header.blade.php
@@ -1,31 +1,6 @@
 <!DOCTYPE html>
 <html>
-<head>
-    {{-- Configuration --}}
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="@yield('description')">
-    <title>@yield('title')</title>
-    @if($page=='home')
-        <link href="{{asset("public/css/animation.css")}}" rel="stylesheet" type="text/css"/>
-    @endif
-    <link href="{{asset("public/css/app.css")}}" rel="stylesheet" type="text/css"/>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.2/dist/alpine.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    {{-- Favicon --}}
-    <link rel="apple-touch-icon" sizes="180x180" href="{{asset("public/favicon/apple-touch-icon.png")}}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{asset("public/favicon/favicon-32x32.png")}}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{asset("public/favicon/favicon-16x16.png")}}">
-    <link rel="manifest" href="{{asset("public/favicon/site.webmanifest")}}">
-    {{-- Fonts --}}
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-        rel="stylesheet">
-</head>
+
 <body>
 <header id="header">
     <nav class="container">
