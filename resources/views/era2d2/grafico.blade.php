61312f46f1c5e51ab7d50894b28c6b75dd3ad685
asllow <arthurmir0429@gmail.com>
2024-12-06 14:47:57 -0300

    k

--- resources/views/era2d2/grafico.blade.php
+++ resources/views/era2d2/grafico.blade.php
@@ -1,9 +1,15 @@
 @extends('era2d2.header')
 
-{{ $title = "Gráfico de " . ucfirst($selector) }}
-@section('title', $title)
+@section('title', "Gráfico de " . ucfirst($selector))
 
-@section('description', '')
+@section('description', 'Essa página mostra os últimos resultados de uma das nossas pesquisas associadas à medição')
 
 @section('main')
+    <main id="main">
+        <section class="section" id="home">
+            <div class="container">
+                <h1 class="title">Gráfico de {{ ucfirst($selector) }}</h1>
+            </div>
+        </section>
+    </main>
 @endsection
