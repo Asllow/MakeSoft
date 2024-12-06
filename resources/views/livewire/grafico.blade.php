871203c6b24c21de852aa0b14bccc7df8752182a
asllow <arthurmir0429@gmail.com>
2024-12-06 15:28:10 -0300

    k

--- resources/views/livewire/grafico.blade.php
+++ resources/views/livewire/grafico.blade.php
@@ -1,24 +1,26 @@
-<script type="text/javascript">
-    google.charts.load('current', {'packages': ['corechart']});
-    google.charts.setOnLoadCallback(drawChart);
+<div>
+    <script type="text/javascript">
+        google.charts.load('current', {'packages': ['corechart']});
+        google.charts.setOnLoadCallback(drawChart);
 
-    function drawChart() {
-        var data = google.visualization.arrayToDataTable([
-            ['Year', 'Sales', 'Expenses'],
-            ['2004', 1000, 400],
-            ['2005', 1170, 460],
-            ['2006', 660, 1120],
-            ['2007', 1030, 540]
-        ]);
+        function drawChart() {
+            var data = google.visualization.arrayToDataTable([
+                ['Year', 'Sales', 'Expenses'],
+                ['2004', 1000, 400],
+                ['2005', 1170, 460],
+                ['2006', 660, 1120],
+                ['2007', 1030, 540]
+            ]);
 
-        var options = {
-            title: 'Company Performance',
-            curveType: 'function',
-            legend: {position: 'bottom'}
-        };
+            var options = {
+                title: 'Company Performance',
+                curveType: 'function',
+                legend: {position: 'bottom'}
+            };
 
-        var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+            var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
 
-        chart.draw(data, options);
-    }
-</script>
+            chart.draw(data, options);
+        }
+    </script>
+</div>
