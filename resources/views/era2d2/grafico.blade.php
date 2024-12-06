e3ee872bc5829dcc00cde20b2b292af598ef07b6
asllow <arthurmir0429@gmail.com>
2024-12-06 19:48:28 -0300

    k

--- resources/views/era2d2/grafico.blade.php
+++ resources/views/era2d2/grafico.blade.php
@@ -1,6 +1,6 @@
 @extends('era2d2.header')
 
-@section('title', "Gráfico de ")
+@section('title', "Gráfico de " . ucfirst($selector))
 
 @section('description', 'Essa página mostra os últimos resultados de uma das nossas pesquisas associadas à medição')
 
