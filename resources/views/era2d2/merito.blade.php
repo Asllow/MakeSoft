a1205a1155c5abda92f780ef7cf88a28ee53e360
asllow <arthurmir0429@gmail.com>
2024-12-05 23:23:02 -0300

    First Changes

--- resources/views/era2d2/merito.blade.php
+++ resources/views/era2d2/merito.blade.php
@@ -32,12 +32,67 @@
                 </div>
                 <div class="divider1"></div>
                 <div class="section-pdf">
-                    <p class="subtitle">Aqui, você verá o nosso projeto e escrita para o Mérito Científico</p>
+                    <h2 class="subtitle">Aqui, você verá o nosso projeto e escrita para o Mérito Científico para o TBR
+                        2024</h2>
+                    <i class="bi bi-arrow-down-circle i-color"></i>
                     <div class="pdf">
-
+                        <div id="test-id-1" style="text-align: center; width: 100%; height: 100%" class="embed-pdf"
+                             data-url="https://makesoft.net.br/public/pdf/teste.pdf">
+                            <div id="loading" class="spinner-border" role="status">
+                                <span class="sr-only">Loading...</span>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </section>
     </main>
+    <script>
+        $(document).ready(function () {
+
+            let embed_pdfs = {};
+
+            // Criação dos visualizadores de PDF
+            $('.embed-pdf').each(function () {
+                const $pdfViewer = $('<iframe style="width: 100%; height: 100%" frameborder="0" scrolling="no"></iframe>');
+                $pdfViewer.appendTo($(this));
+                console.log($(this).attr('id') + " created");
+                embed_pdfs[$(this).attr('id')] = 'created';
+
+                // Inicia o carregamento do PDF
+                const pdfUrl = $(this).data('url');
+                $pdfViewer.attr('src', 'https://docs.google.com/viewer?url=' + pdfUrl + '&embedded=true');
+                console.log("Carregamento iniciado para o PDF em: " + pdfUrl);
+
+                // Configurar evento de carregamento completo
+                $pdfViewer.on('load', function () {
+                    embed_pdfs[$(this).parent('.embed-pdf').attr('id')] = 'loaded';
+                    $(this).siblings('.loader').remove(); // Remove o loader, se existir
+                    $('#loading').css('display', 'none'); // Oculta o ícone ao finalizar o carregamento
+                    console.log($(this).parent('.embed-pdf').attr('id') + " fully loaded");
+                });
+            });
+
+            // Monitoramento e tentativa de recarregar PDFs com falha
+            let embed_pdf_check = setInterval(function () {
+                let remaining_embeds = 0;
+                $.each(embed_pdfs, function (key) {
+                    try {
+                        // Verifica se o iframe está vazio
+                        if ($($('#' + key)).find('iframe').contents().find("body").contents().length === 0) {
+                            remaining_embeds++;
+                            console.log(key + " resetting");
+                            $($('#' + key)).find('iframe').attr('src', 'https://docs.google.com/viewer?url=' + $('#' + key).data('url') + '&embedded=true');
+                        }
+                    } catch (err) {
+                        console.log(key + " reloading");
+                    }
+                });
+
+                if (!remaining_embeds) {
+                    clearInterval(embed_pdf_check); // Interrompe o intervalo quando todos os PDFs carregaram
+                }
+            }, 1000);
+        });
+    </script>
 @endsection
