c118dd3216d13113b7652144f41fbba9cea3d824
asllow <arthurmir0429@gmail.com>
2024-12-06 08:41:53 -0300

    First Changes

---
+++ database/migrations/2024_12_06_112647_ph.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::connection('makesoft')->create('ph', function (Blueprint $table) {
+            $table->id();
+            $table->float('valor', 3);
+            $table->timestamp('created_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::connection('makesoft')->dropIfExists('ph');
+    }
+};
