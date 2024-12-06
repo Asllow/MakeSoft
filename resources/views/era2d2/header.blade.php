19b26bbe46fb8fd1a1862280f3710af5097c3852
asllow <arthurmir0429@gmail.com>
2024-12-05 23:56:06 -0300

    First Changes

--- resources/views/era2d2/header.blade.php
+++ resources/views/era2d2/header.blade.php
@@ -14,10 +14,6 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.2/dist/alpine.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-            crossorigin="anonymous">
-    </script>
     {{-- Favicon --}}
     <link rel="apple-touch-icon" sizes="180x180" href="{{asset("public/favicon/apple-touch-icon.png")}}">
     <link rel="icon" type="image/png" sizes="32x32" href="{{asset("public/favicon/favicon-32x32.png")}}">
