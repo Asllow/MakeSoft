297d8a1973aabe680d9e1fd7032bc60981dffc64
asllow <arthurmir0429@gmail.com>
2024-12-06 17:15:05 -0300

    k

--- resources/views/era2d2/grafico.blade.php
+++ resources/views/era2d2/grafico.blade.php
@@ -7,7 +7,7 @@
 @section('main')
     <main id="main">
         <section class="section" id="head">
-            <div class="container">
+            <div class="container grafico-container">
                 <div class="head">
                     <a href="{{ route('era.index'). '#' . $selector }}"><i class="bi bi-arrow-left-circle"></i></a>
                     <h2 class="title subtitle">Gráfico de {{ ucfirst($selector) }}</h2>
