abd41eb5e3e5ac6c4265b9f1ff0b097e81bf715f
asllow <arthurmir0429@gmail.com>
2024-12-08 22:00:38 -0300

    k

--- app/Livewire/Grafico.php
+++ app/Livewire/Grafico.php
@@ -43,8 +43,8 @@ use Livewire\Component;
             default:
                 return abort(404);
         }
-        $this->chart_label = json_encode($model::select('id')->latest()->take($results)->get()->pluck('id')->toArray());
-        $this->chart_data = json_encode($model::select('valor')->latest()->take($results)->get()->pluck('valor')->toArray());
+        $this->chart_label = $model::select('id')->latest()->take($results)->get()->pluck('id')->toArray();
+        $this->chart_data = $model::select('valor')->latest()->take($results)->get()->pluck('valor')->toArray();
         $this->dispatch('chart-updated', [
             'chartData' => $this->chart_data,
             'xAxisData' => $this->chart_label
