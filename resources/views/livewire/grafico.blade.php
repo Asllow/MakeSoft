492349be4ca1ba2deffa0158deb10058e2c6d999
asllow <arthurmir0429@gmail.com>
2024-12-09 10:14:23 -0300

    k

--- resources/views/livewire/grafico.blade.php
+++ resources/views/livewire/grafico.blade.php
@@ -29,8 +29,6 @@
                         zoom: {
                             enabled: false
                         },
-                        height: "100%",
-                        width: "100%",
                     },
                     series: [{
                         name: '{{ $selector }}',
