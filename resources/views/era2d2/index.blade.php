c6ae17ebad8d953439364883f92940023bb4dc49
asllow <arthurmir0429@gmail.com>
2024-12-10 11:49:37 -0300

    k

--- resources/views/era2d2/index.blade.php
+++ resources/views/era2d2/index.blade.php
@@ -12,12 +12,13 @@
                 <h2 class="title subtitle">Monitoramento em tempo real da qualidade da água</h2>
                 <div class="project-presentation">
                     <div class="text">
-                        <p>Essa é uma apresentação do nosso projeto para o Torneio TBR 2024. A equipe ERA2-D2
+                        <p>&emsp;Essa é uma apresentação do nosso projeto para o Torneio TBR 2024. A equipe ERA2-D2
                             desenvolveu uma plataforma de monitoramento aquático com um propósito claro: contribuir para
                             a preservação dos recursos hídricos e promover um futuro sustentável. Assim, essa plataforma
                             é capaz de registrar parâmetros como pH, turbidez, níveis de oxigênio dissolvido,
                             temperatura e condutividade.</p>
-                        <p>Inspirada no 6º Objetivo de Desenvolvimento Sustentável da ONU (Água Potável e Saneamento), a
+                        <p>&emsp;Inspirada no 6º Objetivo de Desenvolvimento Sustentável da ONU (Água Potável e
+                            Saneamento), a
                             plataforma utiliza sensores para coletar dados em tempo real sobre parâmetros essenciais da
                             qualidade da água. Esses dados estão disponíveis para visualização e análise da qualidade da
                             água, facilitando a tomada de decisões informadas, a implementação de medidas corretivas e o
