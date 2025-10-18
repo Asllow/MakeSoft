a094b91a90baa5a25a564f1a9575514e32112797
daniellyborges2024-collab <daniellyborges2024@gmail.com>
2025-10-18 15:18:31 -0300

    Update title and subtitle in index.blade.php

--- resources/views/era2d2/index.blade.php
+++ resources/views/era2d2/index.blade.php
@@ -8,8 +8,9 @@
     <main id="main">
         <section class="section" id="home">
             <div class="container page1">
-                <h1 class="title">Rios de Dados</h1>
-                <h2 class="title subtitle">Monitoramento em tempo real da qualidade da água</h2>
+              <h1 class="title">Terra Viva</h1>
+<h2 class="title subtitle">Monitoramento e preservação dos ecossistemas terrestres</h2>
+
                 <div class="project-presentation">
                     <div class="text">
                         <p>&emsp;A ERA2-D2 é formada por alunos do CEFET-MG – Campus Divinópolis e foi criada em 2023 pelo grupo Estudos de Robótica e Automação (ERA).
