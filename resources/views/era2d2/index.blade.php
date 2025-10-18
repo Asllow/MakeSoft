3bee119aade4121976c1c25130b9f6098eb3e0d7
daniellyborges2024-collab <daniellyborges2024@gmail.com>
2025-10-18 14:47:36 -0300

    Revise project description for ERA2-D2
    Updated project description to include team achievements and focus.

--- resources/views/era2d2/index.blade.php
+++ resources/views/era2d2/index.blade.php
@@ -12,11 +12,9 @@
                 <h2 class="title subtitle">Monitoramento em tempo real da qualidade da água</h2>
                 <div class="project-presentation">
                     <div class="text">
-                        <p>&emsp;cu é uma apresentação do nosso projeto para o Torneio TBR 2024. A equipe ERA2-D2
-                            desenvolveu uma plataforma de monitoramento aquático com um propósito claro: contribuir para
-                            a preservação dos recursos hídricos e promover um futuro sustentável. Assim, essa plataforma
-                            é capaz de registrar parâmetros como pH, turbidez, níveis de oxigênio dissolvido,
-                            temperatura e condutividade.</p>
+                        <p>&emsp;A ERA2-D2 é formada por alunos do CEFET-MG – Campus Divinópolis e foi criada em 2023 pelo grupo Estudos de Robótica e Automação (ERA).
+Em 2024, conquistou 1º lugar em Mérito Científico e, em 2025, chega com uma formação totalmente nova, composta por nove integrantes e orientada pelo técnico João Luiz Vieira e pelo mentor Diêgo Fernandes da Cruz.
+O grupo busca aperfeiçoar o desempenho técnico e fortalecer suas competências em programação, eletrônica e trabalho em equipe.</p>
                         <p>&emsp;Inspirada no 6º Objetivo de Desenvolvimento Sustentável da ONU (Água Potável e
                             Saneamento), a
                             plataforma utiliza sensores para coletar dados em tempo real sobre parâmetros essenciais da
