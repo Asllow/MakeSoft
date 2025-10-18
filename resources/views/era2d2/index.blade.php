b16c5c8b12c2c2a59765c8ba9a266859e3c8b9a7
daniellyborges2024-collab <daniellyborges2024@gmail.com>
2025-10-18 19:08:11 -0300

    Refactor HTML structure in index.blade.php
    Removed redundant line breaks and cleaned up HTML structure.

--- resources/views/era2d2/index.blade.php
+++ resources/views/era2d2/index.blade.php
@@ -37,8 +37,14 @@
                     <p>Somos o ERA2D2, uma equipe de robótica formada por estudantes do ensino médio do CEFET-MG, em Divinópolis, Minas Gerais. Nossa jornada começou no início de 2023, dentro do grupo Estudos de Robótica e Automação (ERA), com o sonho de competir na categoria High do Torneio Brasil de Robótica (TBR).</p>
                     <p>O nome ERA2D2 nasceu em uma de nossas primeiras reuniões, quando decidimos unir nossa identidade com o ERA à paixão de muitos de nós por Star Wars. Assim, escolhemos homenagear o icônico e carismático R2D2, que agora também inspira nossa criatividade e espírito de equipe.</p>
              <br 
+                 br>
               <br
-                  <br
+                  br>
+                     <br
+                  br>
+                     <br
+                  br>
+                 
             </div>
             </div>
         </div>
