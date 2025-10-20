94db6f6ae5283fe414a0d8e0ed715b891a6a76c0
daniellyborges2024-collab <daniellyborges2024@gmail.com>
2025-10-20 15:35:40 -0300

    Update merito.blade.php

--- resources/views/era2d2/merito.blade.php
+++ resources/views/era2d2/merito.blade.php
@@ -29,7 +29,7 @@
                     <i class="bi bi-arrow-down-circle i-color"></i>
                     <div class="pdf">
                         <div id="test-id-1" style="text-align: center; width: 100%; height: 100%" class="embed-pdf"
-                             data-url="https://makesoftsolutions.com.br/public/pdf/merito.pdf">
+                             data-url="https://makesoftsolutions.com.br/public/pdf/MERITO2025.pdf">
                             <div id="loading" class="spinner-border" role="status">
                                 <span class="sr-only">Carregando...</span>
                             </div>
