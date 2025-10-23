5d2a73f84e5c3886d8777df112a11dc9de10ae6d
daniellyborges2024-collab <daniellyborges2024@gmail.com>
2025-10-23 08:22:03 -0300

    Update organi.blade.php

--- resources/views/era2d2/organi.blade.php
+++ resources/views/era2d2/organi.blade.php
@@ -55,16 +55,47 @@
 
             let embed_pdfs = {};
 
+            // Criação dos visualizadores de PDF
             $('.embed-pdf').each(function () {
                 const $pdfViewer = $('<iframe style="width: 100%; height: 100%" frameborder="0" scrolling="no"></iframe>');
                 $pdfViewer.appendTo($(this));
+                console.log($(this).attr('id') + " created");
+                embed_pdfs[$(this).attr('id')] = 'created';
+
+                // Inicia o carregamento do PDF
                 const pdfUrl = $(this).data('url');
                 $pdfViewer.attr('src', 'https://docs.google.com/viewer?url=' + pdfUrl + '&embedded=true');
+                console.log("Carregamento iniciado para o PDF em: " + pdfUrl);
 
+                // Configurar evento de carregamento completo
                 $pdfViewer.on('load', function () {
-                    $('#loading').hide();
+                    embed_pdfs[$(this).parent('.embed-pdf').attr('id')] = 'loaded';
+                    $(this).siblings('.loader').remove(); // Remove o loader, se existir
+                    $('#loading').css('display', 'none'); // Oculta o ícone ao finalizar o carregamento
+                    console.log($(this).parent('.embed-pdf').attr('id') + " fully loaded");
                 });
             });
+
+            // Monitoramento e tentativa de recarregar PDFs com falha
+            let embed_pdf_check = setInterval(function () {
+                let remaining_embeds = 0;
+                $.each(embed_pdfs, function (key) {
+                    try {
+                        // Verifica se o iframe está vazio
+                        if ($($('#' + key)).find('iframe').contents().find("body").contents().length === 0) {
+                            remaining_embeds++;
+                            console.log(key + " resetting");
+                            $($('#' + key)).find('iframe').attr('src', 'https://docs.google.com/viewer?url=' + $('#' + key).data('url') + '&embedded=true');
+                        }
+                    } catch (err) {
+                        console.log(key + " reloading");
+                    }
+                });
+
+                if (!remaining_embeds) {
+                    clearInterval(embed_pdf_check); // Interrompe o intervalo quando todos os PDFs carregaram
+                }
+            }, 1000);
         });
     </script>
 @endsection
