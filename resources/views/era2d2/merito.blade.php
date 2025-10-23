f9129fdf2cdfb0ef2bae56f5b4daae5e6eeba82d
daniellyborges2024-collab <daniellyborges2024@gmail.com>
2025-10-23 07:56:38 -0300

    Update merito.blade.php

--- resources/views/era2d2/merito.blade.php
+++ resources/views/era2d2/merito.blade.php
@@ -1,32 +1,34 @@
 @extends('era2d2.header')
 
-@section('title', 'Organização e Método')
+@section('title', 'Mérito Científico')
 
-@section('description', 'Aqui está um resumo do que é a organização e método e do nosso trabalho')
+@section('description', 'Aqui está um resumo do que é o mérito científico e do nosso trabalho')
 
 @section('main')
     <main id="main">
         <section class="section4">
             <div class="container grid">
                 <div class="page-header">
-                    <h1 class="title">Organização e Método</h1>
+                    <h1 class="title">Mérito Científico</h1>
                     <div class="text">
-                        <p>&emsp;A equipe de Organização e Método é um subgrupo do ERA2-D2 para o Torneio Brasil de
-                            Robótica
-                            (TBR), com o objetivo principal de garantir a eficiência e a eficácia das operações e
-                            atividades do grupo, gerenciando a equipe de forma eficaz.</p>
-                        <p>&emsp;Para a categoria High (de 15 a 19 anos), o TBR desafiou a equipe a ter uma boa gestão
-                            da
-                            temporada. A subequipe de Organização e Método decidiu desenvolver um Plano de Ação Para a
-                            Temporada, distribuindo as tarefas em um cronograma detalhado que inclui as ações a serem
-                            realizadas, os prazos de realização, os responsáveis pelas atividades, os insumos
-                            necessários e os resultados esperados. A equipe também mantém uma comunicação eficaz e um
-                            acompanhamento contínuo do progresso das atividades.</p>
-                        <p>&emsp;Além disso, a equipe de Organização e Método destaca a importância de um planejamento
-                            estratégico bem definido e da adoção de métodos ágeis para lidar com imprevistos e mudanças
-                            de escopo. A implementação dessas práticas contribui para a melhoria contínua dos processos,
-                            assegurando que a equipe esteja sempre alinhada com os objetivos do TBR e preparada para
-                            enfrentar os desafios da temporada.</p>
+                        <p>&emsp;A equipe do Mérito Científico é um subgrupo do ERA2-D2 para o Torneio Brasil de
+                            Robótica,
+                            onde o objetivo principal é a extensão de pesquisas aprofundadas em tecnologia e robótica,
+                            permitindo que os alunos pratiquem os conhecimentos adquiridos durante o curso. Nesse
+                            sentido, relacionado ao Objetivo de Desenvolvimento Sustentável 6 (ODS 6) das Nações Unidas
+                            como proposta para a categoria High (de 15 a 19 anos), o TBR desafiou a criação de soluções
+                            para proteger e restaurar sistemas relacionados à água.</p>
+                        <p>&emsp;O Mérito, a partir de recursos viáveis e acessíveis, decidiu criar uma plataforma que
+                            monitorasse os parâmetros qualitativos aquáticos, sendo eles, condutividade, pH, temperatura
+                            e a turbidez. Sendo assim, o monitoramento contínuo desses parâmetros é crucial para a
+                            identificação de possíveis contaminações, a gestão adequada de recursos hídricos e a
+                            preservação da vida aquática, visando garantir a integridade dos recursos naturais e a
+                            qualidade de vida das futuras gerações. Além disso, a equipe reafirma a importância de
+                            tecnologias alcançáveis para comunidades e regiões em condições de difícil acesso aos
+                            recursos hídricos ou em locais que estão suscetíveis à poluição.</p>
+                        <p>&emsp;Portanto, o Mérito Científico não apenas busca cumprir o desafio do TBR, mas também
+                            contribuir de forma significativa para alcançar metas globais de sustentabilidade,
+                            promovendo a conservação dos recursos hídricos para as futuras gerações.</p>
                     </div>
                 </div>
             </div>
@@ -35,12 +37,12 @@
         <section class="section3">
             <div class="container grid">
                 <div class="section-pdf">
-                    <h2 class="subtitle">Aqui, você verá o nosso projeto e escrita da Organização e Método para o TBR
+                    <h2 class="subtitle">Aqui, você verá o nosso projeto e escrita do Mérito Científico para o TBR
                         2024</h2>
                     <i class="bi bi-arrow-down-circle i-color"></i>
                     <div class="pdf">
                         <div id="test-id-1" style="text-align: center; width: 100%; height: 100%" class="embed-pdf"
-                             data-url="https://makesoftsolutions.com.br/public/pdf/organi.pdf">
+                             data-url="https://makesoftsolutions.com.br/public/pdf/merito.pdf">
                             <div id="loading" class="spinner-border" role="status">
                                 <span class="sr-only">Carregando...</span>
                             </div>
