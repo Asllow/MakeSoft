72891808ab12baaf9030ea6742612388fe8b049c
daniellyborges2024-collab <daniellyborges2024@gmail.com>
2025-11-13 07:31:12 -0300

    Update organi.blade.php

--- resources/views/era2d2/organi.blade.php
+++ resources/views/era2d2/organi.blade.php
@@ -11,12 +11,22 @@
                 <div class="page-header">
                     <h1 class="title">Organização e Método</h1>
                     <div class="text">
-                       <p>&emsp;A equipe de Organização e Método é um subgrupo do ERA2-D2 no Torneio Brasil de Robótica (TBR), responsável por garantir que as atividades da equipe sejam realizadas de forma organizada, eficiente e colaborativa ao longo da temporada 2025. Nesta edição, com o tema “Vida Terrestre”, o grupo buscou desenvolver estratégias que unissem planejamento, comunicação e sustentabilidade.</p>
-
-<p>&emsp;O principal foco da subequipe foi estruturar um cronograma funcional e dinâmico, que organizasse as tarefas, prazos, responsáveis e recursos necessários para cada etapa do projeto. O planejamento foi elaborado com base em metodologias ágeis, permitindo que o grupo se adaptasse rapidamente às mudanças e desafios encontrados durante o desenvolvimento do robô e das apresentações.</p>
-
-<p>&emsp;Além disso, a equipe adotou práticas que incentivam a cooperação e a responsabilidade entre os membros, garantindo que todos compreendessem seus papéis dentro do ERA2-D2. Dessa forma, a Organização e Método contribui para o crescimento coletivo, promovendo uma gestão eficiente, participativa e alinhada aos valores do TBR 2025 e ao compromisso com a sustentabilidade e a preservação da vida terrestre.</p>
-
+                        <p>&emsp;A equipe de Organização e Método é um subgrupo do ERA2-D2 para o Torneio Brasil de
+                            Robótica
+                            (TBR), com o objetivo principal de garantir a eficiência e a eficácia das operações e
+                            atividades do grupo, gerenciando a equipe de forma eficaz.</p>
+                        <p>&emsp;Para a categoria High (de 15 a 19 anos), o TBR desafiou a equipe a ter uma boa gestão
+                            da
+                            temporada. A subequipe de Organização e Método decidiu desenvolver um Plano de Ação Para a
+                            Temporada, distribuindo as tarefas em um cronograma detalhado que inclui as ações a serem
+                            realizadas, os prazos de realização, os responsáveis pelas atividades, os insumos
+                            necessários e os resultados esperados. A equipe também mantém uma comunicação eficaz e um
+                            acompanhamento contínuo do progresso das atividades.</p>
+                        <p>&emsp;Além disso, a equipe de Organização e Método destaca a importância de um planejamento
+                            estratégico bem definido e da adoção de métodos ágeis para lidar com imprevistos e mudanças
+                            de escopo. A implementação dessas práticas contribui para a melhoria contínua dos processos,
+                            assegurando que a equipe esteja sempre alinhada com os objetivos do TBR e preparada para
+                            enfrentar os desafios da temporada.</p>
                     </div>
                 </div>
             </div>
@@ -26,11 +36,11 @@
             <div class="container grid">
                 <div class="section-pdf">
                     <h2 class="subtitle">Aqui, você verá o nosso projeto e escrita da Organização e Método para o TBR
-                        2025</h2>
+                        2024</h2>
                     <i class="bi bi-arrow-down-circle i-color"></i>
                     <div class="pdf">
                         <div id="test-id-1" style="text-align: center; width: 100%; height: 100%" class="embed-pdf"
-                             data-url="https://makesoftsolutions.com.br/public/pdf/ORGANI2025.pdf">
+                             data-url="https://makesoftsolutions.com.br/public/pdf/organi.pdf">
                             <div id="loading" class="spinner-border" role="status">
                                 <span class="sr-only">Carregando...</span>
                             </div>
