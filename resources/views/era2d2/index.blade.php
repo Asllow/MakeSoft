88afa72075d3e1590277c47614fd9b94bd3db20b
daniellyborges2024-collab <daniellyborges2024@gmail.com>
2025-11-14 13:50:41 -0300

    Update index.blade.php

--- resources/views/era2d2/index.blade.php
+++ resources/views/era2d2/index.blade.php
@@ -118,7 +118,7 @@
                                 pressão humana sobre os recursos naturais.</p>
                         </div>
                         <div class="hold-button button-temperatura">
-                            <a class="button" href="{{ route('era.grafico', 'temperatura') }}">Gráfico de
+                            <a class="button" href="{{ route('era.grafico', 'temperatura.') }}">Gráfico de
                                 Temperatura</a>
                         </div>
                     </div>
